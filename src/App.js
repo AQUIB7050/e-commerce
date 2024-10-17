0f6f38d2a6cf0082093c393d69a3b83dbda8b18a
Aquib Ahmed Ansari <54367316+AQUIB7050@users.noreply.github.com>
2024-10-17 22:10:25 +0530

    Order Page Added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Cart from './customer/components/Cart/Cart';
 import Checkout from './customer/components/Checkout/Checkout';
 import Footer from './customer/components/Footer/Footer';
 import Navigation from './customer/components/Navigation/Navigation';
+import Order from './customer/components/Order/Order';
+import OrderDetails from './customer/components/Order/OrderDetails';
 import Product from './customer/components/Product/Product';
 import ProductDetails from './customer/components/ProductDetails/ProductDetails';
 import HomePage from './customer/pages/HomePage';
@@ -16,7 +18,9 @@ function App() {
       {/* <Product/> */}
       {/* <ProductDetails/> */}
       {/* <Cart/> */}
-      <Checkout/>
+      {/* <Checkout/> */}
+      {/* <Order/> */}
+      <OrderDetails/>
      </div>
      <div>
       <Footer/>
