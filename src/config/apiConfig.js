47ef19e1110a0b2b508a9cdbb07cd9b552d2f540
Aquib Ahmed Ansari <54367316+AQUIB7050@users.noreply.github.com>
2024-11-21 14:13:21 +0530

    Update apiConfig.js
    header updated

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-export const API_BASE_URL = "http://localhost:5454";
+//export const API_BASE_URL = "http://localhost:5454";
 //export const API_BASE_URL = "http://ecomverse-backend-production-c9e6.up.railway.app";
-//export const API_BASE_URL = "https://ecomverse-backend-production-c9e6.up.railway.app";
+export const API_BASE_URL = "https://ecomverse-backend-production-c9e6.up.railway.app";
 
 const jwt = localStorage.getItem("jwt");
 
@@ -12,6 +12,7 @@ export const api = axios.create({
     baseURL:API_BASE_URL,
     headers:{
         "Authorization":`Bearer ${jwt}`,
-        "Content-Type":"application/json"
+        /*"Content-Type":"application/json",*/
+        'Content-Type': 'application/x-www-form-urlencoded'
     }
 })
