d888a3ecc06f3bfcdc1cf65623387ec0516b311a
Aquib Ahmed Ansari <54367316+AQUIB7050@users.noreply.github.com>
2024-11-21 11:10:15 +0530

    Update apiConfig.js
    update backend link from http to https

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 //export const API_BASE_URL = "http://localhost:5454";
-export const API_BASE_URL = "http://ecomverse-backend-production-c9e6.up.railway.app";
+//export const API_BASE_URL = "http://ecomverse-backend-production-c9e6.up.railway.app";
+export const API_BASE_URL = "https://ecomverse-backend-production-c9e6.up.railway.app";
 
 const jwt = localStorage.getItem("jwt");
 
